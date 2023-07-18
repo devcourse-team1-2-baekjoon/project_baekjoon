596749e34a4f65fd34b413b2b76e218841a72da6
unknown <daniel9654@ajou.ac.kr>
2023-07-18 15:05:27 +0900

    🎨 [refactoring]: s3_load_RDS_dag

--- dags/dag_test/s3_load_RDS_dag.py
+++ dags/dag_test/s3_load_RDS_dag.py
@@ -1,74 +1,57 @@
-from airflow.decorators import task
 from airflow import DAG
-from datetime import datetime
+from airflow.decorators import task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime, timedelta
+import os
 
-import boto3
-import psycopg2
+# This dag should be triggered after Athena Dag is finished
 
-# AWS 인증 정보
-aws_access_key_id = 'aws_access_key_id'
-aws_secret_access_key = 'aws_secret_access_key'
-
-# PostgreSQL 연결 정보
-db_host = 'db_host'
-db_name = 'db_name'
-db_user = 'db_user'
-db_password = 'db_password'
-
-default_args = {
-    'start_date': datetime(2023, 7, 6),
-    'schedule_interval': None,
-    'catchup': False,
-}
+# s3_load_RDS_dag
+## Task1: list_s3_files: get filenames from s3 bucket
+## Task2: load_csv_to_postgres: load csv files to postgres
+## Dag: task1 >> task2
 
 @task
-def get_s3_files(bucket_name, directory_name=''):
-    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-    objects = s3.list_objects(Bucket=bucket_name, Prefix=directory_name)
-    files = []
-    if 'Contents' in objects:
-        for obj in objects['Contents']:
-            file_key = obj['Key']
-            file_name = file_key.split('/')[-1]
-            if file_name:
-                files.append(file_name)
-    return files
+def list_s3_files():
+    s3_hook = S3Hook(aws_conn_id='aws_conn_id')
+    files = s3_hook.list_keys(bucket_name='bucket_name')
+    csv_files = [file for file in files if file.endswith('.csv')]
+    return csv_files
 
 @task
-def load_csv_to_db(file_name):
-    table_name = 'raw_data.users'
-    s3_bucket_name = 'baekjoon-project-pipeline'
-    directory_name = 'directory_name'
-    s3_file_path = f's3://{s3_bucket_name}/{directory_name}/{file_name}'
+def load_csv_to_postgres(filename):
+    s3_hook = S3Hook(aws_conn_id='aws_conn_id')
+    postgres_hook = PostgresHook(postgres_conn_id='postgres_conn_id')
 
-    copy_query = f"""
-        COPY {table_name}
-        FROM '{s3_file_path}'
-        ACCESS_KEY_ID '{aws_access_key_id}'
-        SECRET_ACCESS_KEY '{aws_secret_access_key}'
-        IGNOREHEADER 1
-        DELIMITER ','
-        CSV;
-    """
+    file_obj = s3_hook.get_key(
+        key=filename,
+        bucket_name='bucket_name'
+    )
 
-    conn = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=db_password)
-    cur = conn.cursor()
-    cur.execute(copy_query)
-    conn.commit()
-    cur.close()
-    conn.close()
+    data = file_obj.get()['Body'].read().decode('utf-8')
+    rows = data.split('\n')
+    rows = rows[1:]
+    rows = [tuple(row.split(',')) for row in rows]
+    table_name = os.path.splitext(filename)[0]
+
+    for row in rows:
+        postgres_hook.run(f"INSERT INTO raw_data.{table_name} VALUES {row};")
 
 with DAG(
-    dag_id='load_csv_to_db',
-    default_args=default_args,
-    description='Load csv data from S3 to RDS',
-    schedule_interval=None,
-    catchup=False,
-) as dag:
+    dag_id = 's3_load_postgre_dag', 
+    start_date=datetime(2023, 7, 18),
+    catchup = False,
+    max_active_runs = 1,
+    default_args = {
+        'retries': 1,
+        'retry_delay': timedelta(minutes=3),
+    }
+    ) as dag:
 
-    list_of_files = get_s3_files('your_bucket_name', 'directory_name')
+    filenames = list_s3_files()
+    load_tasks = [load_csv_to_postgres(filename) for filename in filenames]
 
-    for file in list_of_files:
-        load_csv = load_csv_to_db(file)
+    filenames >> load_tasks
 
-    get_s3_files() >> load_csv_to_db
+
